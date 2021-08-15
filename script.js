551814fcbe5e3943bb01ad3fdbe65580fef2ad84
Loctarogar <igrsl1990@gmail.com>
2021-08-15 19:30:40 +0200

    js basic

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("username");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", function (e) {
+  // Prevents form from being submitted
+  e.preventDefault();
+});
+
+function checkIsEmpty(arr) {}
+
+function checkLength(val) {}
+
+function checkIsIdentical(password, password2) {}
